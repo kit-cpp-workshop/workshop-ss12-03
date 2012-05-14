43465f0d5c771083c99122b5a9fed03cac8209ab
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-05-14 21:48:11 +0200

    Implement line drawing algorithm and some tests

--- simple-drawer/simple-drawer.cpp
+++ simple-drawer/simple-drawer.cpp
@@ -3,35 +3,33 @@
 
 #include <iostream>
 
+int main() {
+    bmp::BatchBitmap24 blackBitmap(100, 100);
 
-int main()
-{
-	bmp::BatchBitmap24 blackBitmap(10, 10);
+    bmp::Color24 blue = { 0, 0, 255 };
 
-	bmp::Color24 blue = {0, 0, 255};
+    for (unsigned int iRow = 0; iRow < blackBitmap.getHeight(); ++iRow) {
+        for (unsigned int iColumn = 0; iColumn < blackBitmap.getWidth(); ++iColumn) {
+            blackBitmap.setPixel(iColumn, iRow, blue);
+        }
+    }
 
-	for(unsigned int iRow = 0; iRow < blackBitmap.getHeight(); ++iRow)
-	{
-		for(unsigned int iColumn = 0; iColumn < blackBitmap.getWidth(); ++iColumn)
-		{
-			blackBitmap.setPixel(iColumn, iRow, blue);
-		}
-	}
+    bmp::AbsoluteCoordinate end1(blackBitmap);
+    bmp::AbsoluteCoordinate end2(blackBitmap);
+    bmp::AbsoluteCoordinate end3(blackBitmap);
+    bmp::AbsoluteCoordinate end4(blackBitmap);
 
-	bmp::AbsoluteCoordinate end(blackBitmap);
-	if(!end.set(9, 5))
-	{
-		std::cout << "error!" << std::endl;
-	}else
-	{
-		bmp::lineto myLine( end.convert() );
-		myLine.applyTo(blackBitmap);
-	}
+    if (!end1.set(90, 50) & !end2.set(30, 70) & !end3.set(5, 30) & !end4.set(99, 0)) {
+        std::cout << "error!" << std::endl;
+    } else {
+        bmp::lineto(end1.convert()).applyTo(blackBitmap);
+        bmp::lineto(end2.convert()).applyTo(blackBitmap);
+        bmp::lineto(end3.convert()).applyTo(blackBitmap);
+        bmp::lineto(end4.convert()).applyTo(blackBitmap);
+    }
 
+    blackBitmap.save("foo.bmp");
 
-	blackBitmap.save("foo.bmp");
-
-
-	return 0;
+    return 0;
 }
 
