1b9c7cd799f122ac212ffd754f3d318ef74e6792
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-05-16 15:20:58 +0200

    Add a polyline implementation

---
+++ simple-drawer/polyline.cpp
@@ -0,0 +1,35 @@
+/*
+ * polyline.cpp
+ *
+ *  Created on: 16.05.2012
+ *      Author: markus
+ */
+
+#include "polyline.h"
+#include "batchbmp.h"
+#include "line.h"
+
+namespace bmp {
+
+    polyline::polyline(RelativeCoordinate * const coords, const unsigned int numCoords) :
+            coords(coords),
+            numCoords(numCoords) {
+        // assert coords != null
+    }
+
+    polyline::~polyline() {
+    }
+
+    bool polyline::applyTo(BatchBitmap24& p_target) {
+        if (numCoords > 0) {
+            p_target.setCurrentPos(coords[0].convert(p_target));
+        }
+
+        for (unsigned int i = 1; i < numCoords; i++) {
+            lineto(coords[i]).applyTo(p_target);
+        }
+
+        return true;
+    }
+
+} /* namespace bmp */
